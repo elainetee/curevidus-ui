bec4c83f5be31f44278ee3ba4538761b092b78fa
elainetee <elainetee88@gmail.com>
2023-01-19 14:41:22 +0800

    improve profile pg ui and add upload pp

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,27 +1,40 @@
 <template>
-  <q-page class="q-ma-xl">
-    <div class="row">
-      <h5>User details</h5>
+  <q-page-card
+    class="fixed-center container-main my-card bg-secondary text-white"
+  >
+    <div id="user-detail-main" class="text-center q-pt-md">
+      <p id="user-detail-tag">User details</p>
     </div>
-    <q-avatar size="200px">
-      <q-img :src="store.user.avatar"/>
-    </q-avatar>
-    <q-card-actions>
-      <label
-        >File
-        <input
-          type="file"
-          id="file"
-          ref="file"
-          v-on:change="handleFileUpload()"
-        />
-      </label>
-      <q-separator dark />
-      <q-btn outline style="color: green" v-on:click="submitFile()"
-        >Upload</q-btn
-      >
-    </q-card-actions>
-    <div class="container row">
+    <div class="text-center">
+      <q-card>
+        <q-avatar size="100px" class="q-mt-sm">
+          <q-img v-if="store.user.avatar != '' "
+            :src="store.user.avatar"
+          />
+          <q-img v-else
+            src="../../public/icons/userdd.png"
+          />
+        </q-avatar>
+        <q-card-actions vertical>
+          <label>
+            <input
+              type="file"
+              style="color: red; width: 200px"
+              id="file"
+              ref="file"
+              class="q-pa-sm"
+              v-on:change="handleFileUpload()"
+            />
+          </label>
+          <q-separator dark />
+          <q-btn outline style="color: green" v-on:click="submitFile()"
+            >Upload</q-btn
+          >
+        </q-card-actions>
+      </q-card>
+    </div>
+
+    <div class="">
       <div class="q-pt-xl">
         <q-markup-table>
           <thead>
@@ -60,7 +73,7 @@
         </div>
       </div>
     </div>
-  </q-page>
+  </q-page-card>
 </template>
 
 <script>
@@ -72,6 +85,7 @@ export default {
     return {
       user: [],
       store,
+      avatar: "",
     };
   },
   methods: {
@@ -87,6 +101,31 @@ export default {
         console.log(error);
       }
     },
+
+    handleFileUpload() {
+      this.avatar = this.$refs.file.files[0];
+    },
+    async submitFile() {
+      // call api to submit
+      let formData = new FormData();
+      formData.append("avatar", this.avatar);
+      try {
+        const res = await this.$axios.post(
+          `http://127.0.0.1:8000/api/user/pp`,
+          formData,
+          {
+            headers: { Authorization: "Bearer" + Cookies.get("token") },
+          }
+        );
+        this.store.user.avatar = `http://127.0.0.1:8000` + res.data.image_url;
+        alert("Profile Picture Updated");
+        // window.location.reload();
+      } catch (e) {
+        var obj = JSON.parse(e.response.data);
+        var res = [];
+        alert(obj.avatar.join(" "));
+      }
+    },
   },
 
   created() {
@@ -96,9 +135,20 @@ export default {
 </script>
 
 <style>
-.container {
+/* .container {
   display: flex;
   justify-content: flex-start;
+} */
+.container-main {
+  width: 30%;
+  border-width: 1px;
+  border-style: solid;
+  padding: 1px;
+}
+
+#user-detail-tag {
+  font-size: 30px;
+  font-weight: bold;
 }
 
 .q-markup-table thead th {
